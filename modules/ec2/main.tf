f681e5778e00c45983e330cf62bef1b8f9e72cc0
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-08-29 22:08:23 +0400

    update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "instance" {
   for_each = var.components
-  ami                  = var.ami
-  instance_type        = var.instance_type
-  key_name             = var.key_name
-  iam_instance_profile = var.iam_instance_profile
 
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  iam_instance_profile   = var.iam_instance_profile
   vpc_security_group_ids = [var.security_group_id]
 
   tags = merge(
@@ -13,23 +13,30 @@ resource "aws_instance" "instance" {
     },
     var.tags
   )
-
 }
 
-# Create a 300 GB EBS Volume for the instance
+# Create a 300 GB EBS Volume for each instance
 resource "aws_ebs_volume" "volume" {
   for_each = var.components
-  availability_zone = aws_instance.instance[each.value.name].availability_zone
+
+  availability_zone = aws_instance.instance[each.key].availability_zone
   size              = 300
-  type = "gp3"
-  encrypted = true
+  type              = "gp3"
+  encrypted         = true
+
   tags = merge(
     {
       Name = "${each.value.name}-volume"
-# Attach the EBS Volume to the instance
+    },
+    var.tags
+  )
+}
+
+# Attach the EBS Volume to each instance
 resource "aws_volume_attachment" "attachment" {
   for_each = var.components
+
   device_name = "/dev/xvdb"
-  volume_id = aws_ebs_volume.volume[each.value.name].id
-  instance_id = aws_instance.instance[each.value.name].id
+  volume_id   = aws_ebs_volume.volume[each.key].id
+  instance_id = aws_instance.instance[each.key].id
 }
