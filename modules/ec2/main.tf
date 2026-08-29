83537135dd721dfb985d2829ac97f8cd9e66c88a
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-08-29 22:19:12 +0400

    key name removed

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "instance" {
 
   ami                    = var.ami
   instance_type          = var.instance_type
-  key_name               = var.key_name
   iam_instance_profile   = var.iam_instance_profile
   vpc_security_group_ids = [var.security_group_id]
 
